0f14f29f4f312ec358dcd20c747735395fa9ab29
Taisiia <taya13taya@gmail.com>
2024-08-28 20:25:05 +0300

    added button component

---
+++ less2/src/components/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({name,action,color}) =>
+    {
+        // const value = "Cancel"
+        return (<><button style = {{backgroundColor:color}}onClick={action}>{name}</button></>)
+    };
+
+    export default Button;
