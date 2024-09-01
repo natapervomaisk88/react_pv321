4adb6f4600e6a8ce8fc4980d80793e92318284e5
MykhailoGrygorenko <osnova2021@gmail.com>
2024-09-01 21:11:34 +0300

    first react hw

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { CityInfoComponent } from "./components/CityInfoCOmponent"
+import { PersonalPageComponent } from "./components/PersonalPageComponent"
+
+
+function App() {
+
+  return (
+    <div className="">
+      <CityInfoComponent/>
+      <PersonalPageComponent/>
+    </div>
+  )
+}
+
+export default App
