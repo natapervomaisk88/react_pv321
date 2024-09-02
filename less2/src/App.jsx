5b579a0841dcfad74ee3b9c3cb378c6982806317
Morrinstal <146762113+Morrinstal@users.noreply.github.com>
2024-09-02 14:20:29 +0200

    Update App.jsx

--- less2/src/App.jsx
+++ less2/src/App.jsx
@@ -1,30 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Card from './components/Card';
+import Guitar from './assets/Guitar.jpg';
 
-function App() {
+const App = () => {
+  const handleAddToCart = () => {
+    alert('Added to cart');
+  };
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="app">
+        <Card
+          image={Guitar}
+          title="Fender Stratocaster"
+          price={199.99}
+          oldPrice={249.99}
+          description="A high-quality guitar made in USA"
+          onAddToCart={handleAddToCart}
+        />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default App;
+
