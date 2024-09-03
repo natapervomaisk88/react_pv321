9223bcab34131ffc640a344a88acf12ee9d5aaad
Taisiia <taya13taya@gmail.com>
2024-09-03 14:51:45 +0300

    Added new branch

--- less2/src/App.jsx
+++ less2/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import "./App.css";
 import Card from './card_components/Card';
 import filledHeart from './card_photo/filledHeart.png'; 
@@ -8,58 +8,78 @@ import photoProduct2 from "./card_photo/Rowenta2.webp";
 import photoProduct3 from "./card_photo/IdeaHome.webp";
 import photoProduct4 from "./card_photo/Dryer.webp";
 
+
 function App() {
     const [isHeartFilled, setIsHeartFilled] = useState(false);
 
-    const handleHeartClick = () => {
-        setIsHeartFilled(!isHeartFilled);
+    const handleHeartClick = (id) => {
+        setIsHeartFilled((prev) => !prev);
     };
+
+    const products = [
+        {
+            id: 1,
+            isHeartFilled: isHeartFilled,
+            filledHeart: filledHeart,
+            emptyHeart: emptyHeart,
+            photo: photoProduct,
+            name: "Фен Rowenta PRO EXPERT CV8820F0",
+            exprice: "$50",
+            price: "$40",
+            action: () => handleHeartClick(1),
+        },
+        {
+            id: 2,
+            isHeartFilled: isHeartFilled,
+            filledHeart: filledHeart,
+            emptyHeart: emptyHeart,
+            photo: photoProduct2,
+            name: "Фен Rowenta x KARL LAGERFELD GLOW ADDICT CV613LF0",
+            exprice: "$60",
+            price: "$50",
+            action: () => handleHeartClick(2),
+        },
+        {
+            id: 3,
+            isHeartFilled: isHeartFilled,
+            filledHeart: filledHeart,
+            emptyHeart: emptyHeart,
+            photo: photoProduct3,
+            name: "Набір для прибирання Idea Home DS-342C Grey-Blue",
+            exprice: "$20",
+            price: "$10",
+            action: () => handleHeartClick(3),
+        },
+        {
+            id: 4,
+            isHeartFilled: isHeartFilled,
+            filledHeart: filledHeart,
+            emptyHeart: emptyHeart,
+            photo: photoProduct4,
+            name: "Сушарка для білизни Eurogold 18 м Чорна (D0503SE)",
+            exprice: "$30",
+            price: "$25",
+            action: () => handleHeartClick(4),
+        },
+    ];
 
     return (
         <div className='cards'>
-            <Card 
-                isHeartFilled={isHeartFilled}
-                filledHeart={filledHeart} 
-                emptyHeart={emptyHeart} 
-                photo = {photoProduct}
-                name="Фен Rowenta PRO EXPERT CV8820F0" 
-                exprice="$50" 
-                price="$40" 
-                action={handleHeartClick}
-            />
-             <Card 
-                isHeartFilled={isHeartFilled}
-                filledHeart={filledHeart} 
-                emptyHeart={emptyHeart} 
-                photo = {photoProduct2}
-                name="Фен Rowenta x KARL LAGERFELD GLOW ADDICT CV613LF0" 
-                exprice="$50" 
-                price="$40" 
-                action={handleHeartClick}
-            />
-             <Card 
-                isHeartFilled={isHeartFilled}
-                filledHeart={filledHeart} 
-                emptyHeart={emptyHeart} 
-                photo = {photoProduct3}
-                name="Набір для прибирання Idea Home DS-342C Grey-Blue" 
-                exprice="$20" 
-                price="$10" 
-                action={handleHeartClick}
-            />
-             <Card 
-                isHeartFilled={isHeartFilled}
-                filledHeart={filledHeart} 
-                emptyHeart={emptyHeart} 
-                photo = {photoProduct4}
-                name="Сушарка для білизни Eurogold 18 м Чорна (D0503SE)" 
-                exprice="$50" 
-                price="$40" 
-                action={handleHeartClick}
-            />
+            {products.map((product) => (
+                <Card
+                    key={product.id}
+                    isHeartFilled={product.isHeartFilled}
+                    filledHeart={product.filledHeart}
+                    emptyHeart={product.emptyHeart}
+                    photo={product.photo}
+                    name={product.name}
+                    exprice={product.exprice}
+                    price={product.price}
+                    action={product.action}
+                />
+            ))}
         </div>
     );
 }
 
 export default App;
-
--- less2/src/card_components/Card.jsx
+++ less2/src/card_components/Card.jsx
@@ -1,27 +1,54 @@
-
 import "./Card.css";
+import React, { useState } from 'react';
+
 const Card = ({ isHeartFilled, filledHeart, emptyHeart, photo, action, name, exprice, price }) => {
+    const [counter, setCounter] = useState(1);
+
+    const handlerUp = () => {
+        setCounter(prev => prev + 1);
+    };
+
+    const handlerDown = () => {
+        setCounter(prev => (prev > 1 ? prev - 1 : 1)); 
+    };
+
+    const numericPrice = parseFloat(price.replace('$', ''));
+    const totalPrice = numericPrice * counter
+
     return (
         <div className="card">
             <img 
-             class = "photo-heart"
+                className="photo-heart"
                 src={isHeartFilled ? filledHeart : emptyHeart} 
                 alt="Heart Icon" 
                 onClick={action} 
             />
             <img 
-            className = "photo-product"
+                className="photo-product"
                 src={photo} 
                 alt={name} 
             />
-            <p className = "name">{name}</p>
-            <p className = "exprice" style={{ textDecoration: 'line-through' }}>{exprice}</p>
-            <p className = "price">{price}</p>
+            <p className="name">{name}</p>
+            <div className="prices">
+                <p className="exprice" style={{ textDecoration: 'line-through' }}>{exprice}</p>
+                <p className="price">{`$${totalPrice}`}</p>
+            </div>
+            <div className="counter">
+                <button className="minusBtn" onClick={handlerDown}>-</button>
+                <input 
+                    type="number" 
+                    className="inputCount" 
+                    value={counter} 
+                    readOnly
+                />
+                <button className="plusBtn" onClick={handlerUp}>+</button>
+            </div>
         </div>
     );
 };
 
 export default Card;
+
 
 
   
