e22cd7817642bc364278b7287b6e3d999afe9ebc
Liza <10252213@stud.op.edu.ua>
2024-09-03 19:11:31 +0300

    Homework3: Updated Cards

--- less3/src/components/Card.jsx
+++ less3/src/components/Card.jsx
@@ -1,10 +1,32 @@
-const Card = ({ image, title, cost }) => {
+import "./Card.css";
+import { useState } from "react";
+
+const Card = ({ id, image, title, cost }) => {
+    const [amount, setAmount] = useState(0);
+
+    const handleIncrementAmount = () => {
+        setAmount(amount + 1);
+    };
+    const handleDencrementAmount = () => {
+        if (amount > 0) {
+            setAmount(amount - 1);
+        }
+    };
+
     return (
         <>
-            <div className="card">
+            <div className="card" id={"card-" + id}>
                 <img className="card-image" src={image} />
-                <p className="card-title">{title}</p>
-                <p className="card-cost">${cost}</p>
+                <div className="card-inner">
+                    <p className="card-title">{title}</p>
+                    <p className="card-cost">${cost.toFixed(2)}</p>
+                    <div className="card-buttons">
+                        <button onClick={handleIncrementAmount}>+</button>
+                        <input type="number" min="0" placeholder="0" readOnly={true} value={amount}></input>
+                        <button onClick={handleDencrementAmount}>-</button>
+                        <p className="card-total">${(cost * amount).toFixed(2)}</p>
+                    </div>
+                </div>
             </div>
         </>
     );
