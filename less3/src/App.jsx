d64acfaf8202d67101b21d47e333439f7e3cb7b8
Natalya Babenko <natapervomaisk88@gmail.com>
2024-09-04 20:56:05 +0300

    added hw and form

--- less3/src/App.jsx
+++ less3/src/App.jsx
@@ -1,20 +1,25 @@
 import "./App.css";
 import { useState, useRef } from "react";
-
+import products from "./model/products.json";
 function App() {
+  console.log(products);
   const [todos, setTodos] = useState([{ id: 1, title: "todo 1" }]);
   const ref_todo = useRef();
 
-  const handleAddTodo = () => {
+  const handleAddTodo = (e) => {
+    e.preventDefault();
     const new_id = todos.length + 1;
     setTodos([...todos, { id: new_id, title: ref_todo.current.value }]);
   };
+
   return (
     <>
-      <input type="text" ref={ref_todo} />
-      <div>
-        <button onClick={handleAddTodo}>Add todo</button>
-      </div>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" ref={ref_todo} />
+        <div>
+          <button>Add todo</button>
+        </div>
+      </form>
       <hr />
       {todos.map((el) => (
         <p key={el.id}>
