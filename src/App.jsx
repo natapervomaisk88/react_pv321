4be9bec3d7fd0b4b936f54c7f18639f88b153f1f
MykhailoGrygorenko <osnova2021@gmail.com>
2024-09-15 22:59:40 +0300

    hw 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { CityInfoComponent } from "./components/CityInfoCOmponent"
-import { PersonalPageComponent } from "./components/PersonalPageComponent"
+import { ProductCardComponent } from "./components/ProductCardComponent";
 
+const products = [
+  {
+    id: 1,
+    title: "Teapot1",
+    price: 30,
+    discount: 20,
+    imagePath: "https://www.rayware.co.uk/media/catalog/product/0/0/0059.098_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=500&width=600&canvas=600:500"
+  },
+  {
+    id: 2,
+    title: "Teapot2",
+    price: 30,
+    discount: 20,
+    imagePath: "https://www.rayware.co.uk/media/catalog/product/0/0/0059.098_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=500&width=600&canvas=600:500"
+  },
+  {
+    id: 3,
+    title: "Teapot3",
+    price: 30,
+    discount: null,
+    imagePath: "https://www.rayware.co.uk/media/catalog/product/0/0/0059.098_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=500&width=600&canvas=600:500"
+  },
+  {
+    id: 4,
+    title: "Teapot4",
+    price: 30,
+    discount: 20,
+    imagePath: "https://www.rayware.co.uk/media/catalog/product/0/0/0059.098_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=500&width=600&canvas=600:500"
+  },
+];
 
 function App() {
 
   return (
-    <div className="">
-      <CityInfoComponent/>
-      <PersonalPageComponent/>
+    <div style={{
+      display: 'flex',
+    }}>
+      {products.map(p => <ProductCardComponent key={p.id} product={p}/>)}
     </div>
   )
 }
